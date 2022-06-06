4ec318a8959c1ed7b80c06e0993ab07f092fb19d
Simon Boisset <sboisset@ambuliz.com>
2022-06-07 01:16:58 +0200

    export types

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { array } from './array';
 import { maybe } from './maybe';
 import { int, number } from './number';
 import { object } from './object';
+import type { Schema, Errors } from './object';
 import { oneOf, string } from './string';
 import { validate } from './validate';
 
 const s = { array, int, maybe, number, object, string, validate, oneOf };
+export type { Schema, Errors };
 export { array, int, maybe, number, object, string, validate, oneOf };
 export default s;
