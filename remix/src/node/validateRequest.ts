325660d67894f9f1495c0b3a78c720fc7a0645da
Simon Boisset <sboisset@ambuliz.com>
2022-06-09 12:44:46 +0200

    fix : Remix type validation

--- remix/src/node/validateRequest.ts
+++ remix/src/node/validateRequest.ts
@@ -2,11 +2,11 @@ import { json } from '@remix-run/node';
 import { Schema } from '@ts-v/core';
 import { getRequestData } from '../data/getRequestData';
 
-export const validateRequest = async <T>(request: Request, schema: Schema<T>) => {
+export const validateRequest = async <T>(request: Request, schema: Schema<T>): Promise<T> => {
   const formData = await getRequestData(request);
   const { data, errors } = schema(formData);
   if (errors) {
     throw json({ errors }, { status: 400 });
   }
-  return data;
+  return data as T;
 };
