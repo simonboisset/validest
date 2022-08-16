0f012e1b3da24eb82a350883fcdc13546b9f96f2
Simon Boisset <sboisset@ambuliz.com>
2022-08-16 14:40:20 +0200

    fix: remove duplicated test utils

--- packages/remix/src/node/validateRequest.test.ts
+++ packages/remix/src/node/validateRequest.test.ts
@@ -1,70 +1,9 @@
-import { Schema } from '@ts-v/core';
-import object from '@ts-v/object';
 import array from '@ts-v/array';
+import object from '@ts-v/object';
+import string from '@ts-v/string';
+import number from '@ts-v/number';
 
 import { validateRequest } from './validateRequest';
-
-export const string =
-  (error?: string): Schema<string> =>
-  (value) =>
-    typeof value === 'string' && value.length > 0 ? { data: value } : { errors: error || 'string' };
-
-export const parseNumber = (value: unknown) => {
-  const n = typeof value === 'string' ? (!value ? undefined : Number(value.replace(',', '.'))) : value;
-  if (typeof n !== 'number' || (n !== 0 && !n)) {
-    return undefined;
-  }
-  return n;
-};
-
-const isOneOf = <T extends string>(options: T[], value: unknown): value is T =>
-  typeof value === 'string' && options.includes(value as T);
-
-export const oneOf =
-  <T extends string>(options: T[], error?: string): Schema<T> =>
-  //@ts-ignore
-  (value: unknown) =>
-    isOneOf(options, value) ? { data: value } : { errors: error || 'oneOf' };
-
-export const date =
-  (error?: string): Schema<Date> =>
-  (value) => {
-    try {
-      if (typeof value === 'string' || typeof value === 'number') {
-        const dateValue = new Date(value);
-        if (dateValue instanceof Date && !isNaN(dateValue.valueOf())) {
-          return { data: dateValue };
-        }
-      } else if (value instanceof Date && !isNaN(value.valueOf())) {
-        return { data: value };
-      }
-      return { errors: error || 'date' };
-    } catch (err) {
-      return { errors: error || 'date' };
-    }
-  };
-
-export const number =
-  (error?: string): Schema<number> =>
-  (value) => {
-    const n = parseNumber(value);
-
-    if (n === undefined) {
-      return { errors: error || 'number' };
-    }
-    return { data: n };
-  };
-
-export const int =
-  (error?: string): Schema<number> =>
-  (value) => {
-    const n = parseNumber(value);
-
-    if (n === undefined || n < 0 || n !== parseInt(n.toString())) {
-      return { errors: error || 'integer' };
-    }
-    return { data: n };
-  };
 
 test('shoud validate form data', async () => {
   const map = new Map([
