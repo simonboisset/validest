dcbf2eed6fb03523bb4af9c10e03ddd11ff1aa50
Simon Boisset <sboisset@ambuliz.com>
2022-09-30 17:29:12 +0200

    feat: Add InferSchema type

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,2 +1,3 @@
 export type TsvError<T> = T extends Record<string, any> ? { [K in keyof T]?: TsvError<T[K]> } : string;
 export type Schema<T> = (data: unknown) => { error: TsvError<T>; data: undefined } | { error: undefined; data: T };
+export type InferSchema<T> = T extends Schema<infer S> ? S : never;
