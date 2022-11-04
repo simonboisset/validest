d2627058bea173d8ba37cda0957055b09462cdfd
Simon Boisset <simonboisset.dev@gmail.com>
2022-11-04 01:38:44 +0100

    fix: Monorepo version get next version

--- utils/monorepo-version/src/getNextVersion.ts
+++ utils/monorepo-version/src/getNextVersion.ts
@@ -2,9 +2,9 @@ const getNextVersion = (version: string, mode: string) => {
   const [M, m, p] = version.split('.').map(Number);
   switch (mode) {
     case 'major':
-      return `${M + 1}.${m}.${p}`;
+      return `${M + 1}.0.0`;
     case 'minor':
-      return `${M}.${m + 1}.${p}`;
+      return `${M}.${m + 1}.0`;
     case 'patch':
       return `${M}.${m}.${p + 1}`;
     default:
