97aefe696580ee9574157e1ef46cca99b27d649a
Simon Boisset <simonboisset.dev@gmail.com>
2022-11-05 13:16:40 +0100

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['prettier'],
-  rules: {
-    'react/jsx-key': 'off',
-  },
-  settings: {
-    next: {
-      rootDir: ['apps/*/'],
-    },
-  },
+  extends: ['eslint:recommended', 'prettier'],
 };
